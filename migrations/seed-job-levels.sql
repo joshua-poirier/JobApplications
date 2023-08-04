ff25aaf47369f16c07edd731eefb3728b2046ed3
joshuaadampoirier <josh.poirier@gmail.com>
2023-08-04 01:24:43 -0400

    seed constants data tables

---
+++ migrations/seed-job-levels.sql
@@ -0,0 +1,43 @@
+/*  Initial populating of the job_levels table.
+***************************************************************************** */
+INSERT INTO public.job_levels
+(
+    job_level
+) 
+VALUES 
+(
+    'Junior'
+),
+(
+    'Intermediate'
+),
+(
+    'Senior'
+),
+(
+    'Lead'
+),
+(
+    'Manager'
+),
+(
+    'Director'
+),
+(
+    'Vice President'
+),
+(
+    'Chief Technology Officer'
+),
+(
+    'Staff'
+),
+(
+    'Principal'
+),
+(
+    'Distinguished'
+),
+(
+    'Fellow'
+);
---
+++ migrations/seed-states.sql
@@ -0,0 +1,22 @@
+/*  Initial populating of the statuses table.
+***************************************************************************** */
+INSERT INTO public.statuses
+(
+    status
+) 
+VALUES 
+(
+    'Submitted'
+),
+(
+    'Interview Scheduled'
+),
+(
+    'Waiting for Feedback'
+),
+(
+    'Closed'
+),
+(
+    'Offer'
+);
